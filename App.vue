8adad944a4684011ecc2c7e43be4e351926d6a0a
Thomas Lindauer <thomas.lindauer@empatix.no>
2018-12-07 09:55:04 +0100

    get network expo host ip

--- App.vue
+++ App.vue
@@ -1,14 +1,47 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">My Vue Native App</text>
+    <text class="text-color-primary">{{sandboxText}}</text>
     </view>
 </template>
+
+<script>
+
+import Expo from "expo";
+
+export default {
+  data: function () {
+    return {
+      sandboxText: ""
+    }
+  },
+  mounted: function () {
+
+    const { manifest } = Expo.Constants;
+    const api = (typeof manifest.packagerOpts === `object`) && manifest.packagerOpts.dev
+    ? manifest.debuggerHost.split(`:`).shift().concat(`:8080`)
+    : `api.example.com`;
+
+    console.log(api+"/test");
+    fetch("http://"+api+"/test").catch(error => {
+      console.log(error);
+    }).then(result => {
+      //console.log(result);
+      result.json().then(jsonres => {
+        console.log(jsonres);
+        this.sandboxText = jsonres;
+      });
+    });
+  
+  }
+}
+</script>
  
 <style>
 .container {
   background-color: white;
   align-items: center;
   justify-content: center;
+  overflow: scroll;
   flex: 1;
 }
 .text-color-primary {
