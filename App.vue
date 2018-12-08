7d438f60ba226691aa41cc0224be8d880d70be58
Thomas Lindauer <thomaslindauer@mail.com>
2018-12-09 00:32:59 +0100

    oauth rest comm

--- App.vue
+++ App.vue
@@ -1,7 +1,27 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">{{sandboxText}}</text>
+
+    <view class="text-container">
+        <text>Vue Native</text>
     </view>
+
+    <text-input
+    :style="{height: 40, borderColor: 'gray', borderWidth: 1}"
+    v-model="username"/>
+
+    <text-input
+    :style="{height: 40, borderColor: 'gray', borderWidth: 1}"
+    v-model="password"/>
+
+    <text class="text-color-primary">{{sandboxText}}</text>
+
+    <button
+    :on-press="registerClientUser"
+    title=""
+    color="#841584"
+    accessibility-label="heyy"/>
+
+  </view>
 </template>
 
 <script>
@@ -9,20 +29,62 @@
 import Expo from "expo";
 
 export default {
+
   data: function () {
     return {
-      sandboxText: ""
+      sandboxText: "",
+      username: "",
+      password: "",
+      api: ""
     }
   },
+
+  methods: {
+
+    registerClientUser: function () {
+
+      console.log(this.username);
+      console.log(this.password);
+
+      fetch('http://'+this.api+'/oauth/token', {
+
+        method: 'POST',
+        headers: {
+        Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({
+
+            grant_type : 'password',
+            client_id : '4',
+            client_secret : 'secret here',
+            username : this.username,
+            password : this.password,
+            scope : ''
+
+        }),
+
+      }).then(response => {
+        console.log(response);
+        response.json().then(jsonresponse => {
+          this.sandboxText = jsonresponse;
+        });
+      });
+
+    }
+
+  },
+
   mounted: function () {
 
     const { manifest } = Expo.Constants;
-    const api = (typeof manifest.packagerOpts === `object`) && manifest.packagerOpts.dev
-    ? manifest.debuggerHost.split(`:`).shift().concat(`:8080`)
+
+    this.api = (typeof manifest.packagerOpts === `object`) && manifest.packagerOpts.dev
+    ? manifest.debuggerHost.split(`:`).shift().concat(`:8000`)
     : `api.example.com`;
 
-    console.log(api+"/test");
-    fetch("http://"+api+"/test").catch(error => {
+    fetch('http://'+this.api+'/api/test').catch(error => {
       console.log(error);
     }).then(result => {
       //console.log(result);
@@ -33,16 +95,14 @@ export default {
     });
   
   }
+
 }
 </script>
  
 <style>
 .container {
   background-color: white;
-  align-items: center;
-  justify-content: center;
-  overflow: scroll;
-  flex: 1;
+  margin-top:10%;
 }
 .text-color-primary {
   color: blue;
